7894ab4b9995106e4ac1fcb4483ff765781c3017
kiRach <kirach@kirach.net>
2012-03-03 00:39:48 +0400

    Added code from first stage

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,10 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Error;
+import play.data.validation.Required;
+import play.data.validation.Valid;
 import play.mvc.*;
-
-import java.util.*;
 
 import models.*;
 
@@ -13,4 +14,29 @@ public class Application extends Controller {
         render();
     }
 
+	public static void calculate(@Required Double q, @Required Double q_big, @Required Double r_big,
+	                             @Required Double h_big, @Required Double n_sh, @Required Double phi,
+	                             @Required Double chi) {
+		//проверяем параметры
+		if (validation.hasErrors()) {
+			params.flash();
+			validation.keep();
+			index();
+		}
+
+		//вектор показателей(концентраций веществ)
+		double y;
+		y = 2.5D * (Math.sqrt(n_sh)) - 0.13D - 0.75 * Math.sqrt(r_big) * (Math.sqrt(n_sh) - 0.1D);
+
+		//коэффициент Шези
+		boolean ifHLessThan5 = h_big <= 5;
+		double c_big = Math.pow(r_big, y) / n_sh;
+
+		//условие для применения метода Фролова-Родзиллера
+		double qByQBig = q / q_big;
+		Logger.info("qByQBig: " + qByQBig);
+		boolean condForFrRodz = (qByQBig >= 0.0025) && (qByQBig <= 0.1);
+
+		render(y, ifHLessThan5, c_big, qByQBig, condForFrRodz, q, q_big, r_big, h_big, n_sh, phi, chi);
+	}
 }
