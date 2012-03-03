8239df44f230d282261bfdd7dff2b2c49d6254e1
kiRach <kirach@kirach.net>
2012-03-03 10:46:41 +0400

    Water body hydraulic conditions coefficient (alpha)

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,6 +6,9 @@ import play.data.validation.Required;
 import play.data.validation.Valid;
 import play.mvc.*;
 
+import java.text.NumberFormat;
+import java.text.DecimalFormat;
+
 import models.*;
 
 public class Application extends Controller {
@@ -38,16 +41,29 @@ public class Application extends Controller {
 		boolean condForFrRodz = (qByQBig >= 0.0025) && (qByQBig <= 0.1);
 
 		//расчет коэффицимета турбулентной диффузии
-		double D;
+		double d_big;
 
 		if (!condForFrRodz){
 			//если НЕ выполняется - то по формуле Краушева
-			D = (9.81 * r_big * h_big * 2.6) / ((0.7 * c_big + 6) * c_big);
+			d_big = (9.81 * r_big * h_big * 2.6) / ((0.7 * c_big + 6) * c_big);
 		} else {
 			//если выполняется - то для летнего времени
-			D = (9.81 * h_big * r_big) / (37 * n_sh * Math.pow(c_big,2));
+			d_big = (9.81 * h_big * r_big) / (37 * n_sh * Math.pow(c_big,2));
 		}
 
-		render(y, ifHLessThan5, c_big, qByQBig, condForFrRodz, q, q_big, r_big, h_big, n_sh, phi, chi, D);
+		NumberFormat nf = NumberFormat.getNumberInstance();
+		DecimalFormat df = (DecimalFormat)nf;
+		df.applyPattern("##0.########");
+		double alpha;
+		double oneByThree = (double)1/(double)3;
+		Logger.info("oneByThree: " + oneByThree);
+		Logger.info("d_big: " + d_big);
+		Logger.info("q squared: " + df.format(Math.pow(q, 2)));
+		double tmp = Math.pow((d_big/Math.pow(q, 2)), oneByThree);
+		Logger.info("tmp:" + tmp);
+		alpha = phi * chi * tmp;
+
+		render(y, ifHLessThan5, c_big, qByQBig, condForFrRodz, q, q_big, r_big, h_big, n_sh, phi, chi, d_big, 
+		       alpha);
 	}
 }
