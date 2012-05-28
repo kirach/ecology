062caafb3fd4bd9ce0888087a25a2c9e097e1712
kiRach <kirach@kirach.net>
2012-05-28 18:40:24 +0400

    Fixed initial script for populatinf db.

--- app/Global.java
+++ app/Global.java
@@ -6,11 +6,23 @@ public class Global extends GlobalSettings {
 	@Override
 	public void onStart(Application app) {
 		Logger.info("Application has started");
-		Substance substance = new Substance();
-		substance.name = "Новое вещество";
-		substance.s_pdk = 1.35;
-		substance.k = 1.75;
-		substance.save();
+		if(Substance.all().size()==0){
+			Substance substance = new Substance();
+			substance.name = "Первое вещество";
+			substance.s_pdk = 1.35;
+			substance.k = 1.75;
+			substance.save();
+			Substance substance2 = new Substance();
+			substance2.name = "Второе вещество";
+			substance2.s_pdk = 1.35;
+			substance2.k = 1.75;
+			substance2.save();
+			Substance substance3 = new Substance();
+			substance3.name = "Третье вещество";
+			substance3.s_pdk = 1.35;
+			substance3.k = 1.75;
+			substance3.save();
+		}
 	}
   
 	@Override
